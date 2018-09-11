90a9ccfed2b10f474d0938620d47ffba6ba60a0d
nanolima <nanolima@nanolima.eti.br>
2018-09-11 14:03:09 -0400

    Update Project

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Escreva uma Mensagem para o Cabeçalho da Página.")
+        self.assertRegex(result.data.decode(), "::: 4 Linux Rules!!! - By Nano Lima :::")
 
 
 if __name__ == "__main__":
